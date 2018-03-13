64bc40b293f6f3727fd242eaa83cde0550dcfc4c
Pedro Baracho <pedropb@trt3.jus.br>
2018-03-13 16:00:13 -0300

    Adding support files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header page-scroll">
+              <a className="navbar-brand page-scroll" href="#page-top">
+                  <img src="images/logo.png" height="30" />
+              </a>
+            </div>
+
+            <div className="collapse navbar-collapse navbar-ex1-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="">Menu item</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </nav>
+
+
+        <section id="intro" className="intro-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <h1><img src="images/logo.png" /></h1>
+                <p>Keep track of all series you have watched or want to watch in the future.</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
